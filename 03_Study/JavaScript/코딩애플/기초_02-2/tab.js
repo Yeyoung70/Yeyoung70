dea1495d0c55746799b79ff4b860291cc41cc1af
yeyoung <doqhd2@gmail.com>
2024-09-30 23:29:31 +0900

    Feat: Add JavaScript_코딩애플_0930

---
+++ 03_Study/JavaScript/코딩애플/기초_02-2/tab.js
@@ -0,0 +1,43 @@
+// 버튼 0 누르면
+// 모든 버튼에 붙은 orange 클래스명 제거
+// 버튼 0에 오렌지 클래스명 추가
+// 모든 div show 클래스명 제거
+// div 0에 show 클래스명 추가
+
+var 버튼 = $('.tab-button');
+
+버튼.eq(0).on('click', function(){
+	버튼.removeClass('orange');
+	버튼.eq(0).addClass('orange');
+	버튼.removeClass('show');
+	버튼.eq(0).addClass('show');
+});
+
+// 버튼 1 누르면
+// 모든 버튼에 붙은 orange 클래스명 제거
+// 버튼 1에 오렌지 클래스명 추가
+// 모든 div show 클래스명 제거
+// div 1에 show 클래스명 추가
+
+// $('.tab-button').eq(1).on('click', function(){
+// 	$('.tab-button').removeClass('orange');
+// 	$('.tab-button').eq(1).addClass('orange');
+// 	$('.tab-content').removeClass('show');
+// 	$('.tab-content').eq(1).addClass('show');
+// });
+
+// $('.tab-button').eq(2).on('click', function(){
+// 	$('.tab-button').removeClass('orange');
+// 	$('.tab-button').eq(2).addClass('orange');
+// 	$('.tab-content').removeClass('show');
+// 	$('.tab-content').eq(2).addClass('show');
+// });
+
+for (let i = 0; i < $('.tab-button').length; i++){
+	버튼.eq(i).on('click', function(){
+		버튼.removeClass('orange');
+		버튼.eq(i).addClass('orange');
+		버튼.removeClass('show');
+		버튼.eq(i).addClass('show');
+	});
+};
